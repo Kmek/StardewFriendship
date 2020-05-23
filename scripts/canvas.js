e99547de92530e8bafcc13c03d1c2b08c7b547db
Kmek <kmekeefe@gmail.com>
2020-05-22 23:53:03 -0400

    Draw axes on graph using drawAxes()

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,6 +1,11 @@
 //******************** Canvas Setup ********************//
 const canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+ctx.lineWidth = 1
+
+// Set origin to bottom corner, like a normal graph
+ctx.translate(0, canvas.height)
+ctx.scale(1, -1)
 
 //******************** Canvas Drawing Functions ********************//
 const draw = {
@@ -12,11 +17,20 @@ const draw = {
         ctx.fillStyle = color;
         ctx.fill();
     },
+    line: function(x1, y1, x2, y2, strokeWidth) {
+        let tempStroke = ctx.lineWidth
+        ctx.lineWidth = strokeWidth
+        ctx.beginPath()
+        ctx.moveTo(x1, y1)
+        ctx.lineTo(x2, y2)
+        ctx.stroke()
+        ctx.lineWidth = tempStroke
+    },
     rect: function(x, y, width, height, color) {
         ctx.beginPath();
         ctx.rect(x, y, width, height);
-        ctx.strokeStyle = color;
-        ctx.stroke();
+        // ctx.strokeStyle = color;
+        // ctx.stroke();
         ctx.fillStyle = color;
         ctx.fill();
     },
@@ -36,6 +50,25 @@ const draw = {
         ctx.stroke()
         ctx.fill()
     },
+    heart: function(x, y, cube) {
+        // DRAW PIXILATED HEART MATCHING STARDEW
+        // Outside #6a0005
+        draw.rect(x - (cube * 4), y - (cube * 0), cube * 7, cube * 2, "#6a0005")
+        draw.rect(x - (cube * 3), y - (cube * 1), cube * 2, cube * 4, "#6a0005")
+        draw.rect(x + (cube * 0), y - (cube * 1), cube * 2, cube * 4, "#6a0005")
+        draw.rect(x - (cube * 2), y - (cube * 2), cube * 3, cube * 1, "#6a0005")
+        draw.rect(x - (cube * 1), y - (cube * 3), cube * 1, cube * 1, "#6a0005")
+        // Inside #d83a01
+        draw.rect(x - (cube * 3), y - (cube * 0), cube * 2, cube * 2, "#d83a01")
+        draw.rect(x - (cube * 0), y - (cube * 0), cube * 2, cube * 2, "#d83a01")
+        draw.rect(x - (cube * 2), y - (cube * 1), cube * 3, cube * 2, "#d83a01")
+        draw.rect(x - (cube * 1), y - (cube * 2), cube * 1, cube * 1, "#d83a01")
+        // Inside highlight #f16f53
+        draw.rect(x - (cube * 2), y + (cube * 1), cube * 1, cube * 1, "#f16f53")
+        draw.rect(x + (cube * 1), y + (cube * 1), cube * 1, cube * 1, "#f16f53")
+
+        // draw.circle(x, y, 1, "#000000")
+    },
     erase: function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
     }
@@ -50,4 +83,44 @@ function inCanvas(x, y) {
 }
 
 //******************** Draw axes ********************//
-// todo
+var origin = [20, 20]
+// These vars must be calculated and set before calling drawAxes()
+var maxHearts = 10
+var maxWeeks = 16
+var heartSpacing, weekSpacing
+
+function drawAxes() {
+    heartSpacing = ((canvas.height - (origin[1] * 2)) / maxHearts)
+    // Draw y-axis
+    draw.line(origin[0], 0, origin[0], canvas.height, 1)
+    for (let i = 0; i < maxHearts; i++) { // Heart (250 pt) dashes
+        let level = heartSpacing * (i + 1) + origin[0]
+        level = Math.round(level)
+        draw.line(origin[0] - 12, level, origin[0] + 12, level, 1)
+        if (maxHearts <= 10) { // 50 pt dashes
+            for (let d = 1; d < 5; d++) {
+                let subLevel = level - ((heartSpacing / 5) * d)
+                subLevel = Math.round(subLevel)
+                draw.line(origin[0] - 3, subLevel, origin[0] + 3, subLevel, 1)
+            }
+        }
+        draw.heart(origin[0] + 1, level - 1, 2)
+    }
+
+    weekSpacing = ((canvas.width - (origin[0] * 2)) / maxWeeks)
+    // Draw x-axis
+    draw.line(0, origin[1], canvas.width, origin[1], 1)
+    for (let i = 0; i < maxWeeks; i++) { // Weeks dashes
+        let level = weekSpacing * (i + 1) + origin[1]
+        level = Math.round(level)
+        draw.line(level, origin[1] - 10, level, origin[1] + 10, 1)
+        if (maxWeeks <= 12) { // Days dashes
+            for (let d = 1; d < 7; d++) {
+                let dayLevel = level - ((weekSpacing / 7) * d)
+                dayLevel = Math.round(dayLevel)
+                draw.line(dayLevel, origin[1] - 3, dayLevel, origin[1] + 3, 1)
+            }
+        }
+    }
+}
+drawAxes()
